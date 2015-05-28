8b8ab926bac4378a65448b8ee1df2a50919e5269
Hawk <879421299@qq.com>
2015-05-29 01:18:07 +0800

    init new module lesson 2 for opengl study

---
+++ opengl_lesson_2/src/main/java/com/example/kehaowei/opengl_lesson_2/LessonTwoRenderer.java
@@ -0,0 +1,26 @@
+package com.example.kehaowei.opengl_lesson_2;
+
+import android.opengl.GLSurfaceView;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+/**
+ * Created by kehaowei on 15/5/29.
+ */
+public class LessonTwoRenderer implements GLSurfaceView.Renderer {
+    @Override
+    public void onSurfaceCreated(GL10 gl10, EGLConfig eglConfig) {
+
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl10, int i, int i1) {
+
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl10) {
+
+    }
+}
