3b585dca642cb013bd009593cd9fcd857913a570
Hawk <879421299@qq.com>
2015-05-29 23:34:50 +0800

    [modify] modify the LessonTwoActivity to be a normal activity

--- opengl_lesson_2/src/main/java/com/example/kehaowei/opengl_lesson_2/LessonTwoActivity.java
+++ opengl_lesson_2/src/main/java/com/example/kehaowei/opengl_lesson_2/LessonTwoActivity.java
@@ -1,12 +1,12 @@
 package com.example.kehaowei.opengl_lesson_2;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class LessonTwoActivity extends ActionBarActivity {
+public class LessonTwoActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
